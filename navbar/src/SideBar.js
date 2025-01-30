6c11ef8257f2d755b1563015ca6de2c9c2457feb
Kataki-Takanashi <dev.ali@abdurraheem.com>
2025-01-30 13:16:31 -0500

    feat(navbar): Add CSS

--- navbar/src/SideBar.js
+++ navbar/src/SideBar.js
@@ -1,3 +1,5 @@
+import { BsPlus, BsFillLightningFill, BsGearFill } from "react-icons/bs";
+import { FaFire, FaPoo } from "react-icons/fa";
 
 
 function SideBar() {
@@ -6,20 +8,34 @@ function SideBar() {
     fixed top-0 left-0 h-screen w-16 
     flex flex-col 
     bg-gray-900 text-white shadow-lg">
-        <i>A</i>
-        <i>B</i>
-        <i>C</i>
-        <i>D</i>
-        <i>E</i>
+        <SideBarIcon icon={<FaFire size="28" />}/>
+        <SideBarIcon icon={<BsPlus size="32"/>}/>
+        <SideBarIcon icon={<BsFillLightningFill size="20"/>}/>
+        <SideBarIcon icon={<FaPoo size="20"/>}/>
       
     </div>
   );
 }
 
 
-const SideBarIcon = ({ icon }) => (
-    <div className="sidebar-icon">
+const SideBarIcon = ({ icon, text = ' tooltip 💡' }) => (
+    <div className="sidebar-icon relative flex items-center justify-center 
+        h-12 w-12 mt-2 mb-2 mx-auto shadow-lg
+        bg-gray-800 text-green-500
+        
+        rounded-3xl hover:rounded-xl
+        transition-all duration-300 ease-linear
+
+        hover:bg-green-600 hover:text-white 
+
+        cursor-pointer">
         {icon}
+
+        <span className="sidebar-tooltip absolute w-auto p-2 m-2 min-w-max left-14
+        rounded-md shadow-md
+        text-white bg-gray-900
+        text-xs font-bold
+        transition-all duration-100 origin-left">{text}</span> 
     </div>
 )
 
--- navbar/tailwind.config.js
+++ navbar/tailwind.config.js
@@ -4,6 +4,10 @@ const colors = require('tailwindcss/colors')
 module.exports = {
   mode: 'jit',
   purge: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
